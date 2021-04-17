271a414af21eea5afbb3cc5b31e15b7a1791eb2a
Meshack Victor <meshackf1@gmail.com>
2021-04-17 11:32:02 +0300

    Update

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+<div class="md:w-1/2 md:px-4 xl:px-6 mt-8 md:mt-0 text-center">
+    <span class="w-20 border-t-2 border-solid border-indigo-200 inline-block mb-3"></span>
+    <h5 class="text-xl font-medium uppercase mb-4">Otapp for your Events!</h5>
+    <p class="text-gray-600">Looking for a event solution ? Contact Otapp now and benefit from our services across the country!</p>
+</div>
+
+<div class="md:w-1/2 md:px-4 xl:px-6 mt-8 md:mt-0 text-center">
+    <span class="w-20 border-t-2 border-solid border-indigo-200 inline-block mb-3"></span>
+    <h5 class="text-xl font-medium uppercase mb-4">Contact Us</h5>
+    <p class="text-gray-600 my-2">
+        <i class="fa fa-phone"></i> <a href="tel:255 677 555 999"><span>+ 255 677 555 999 </span></a>
+    </p>
+    <p class="text-gray-600 my-2">
+        <i class="fa fa-envelope"></i> <a href="mailto:info@otapp.net"><span>+ info@otapp.net </span></a>
+    </p>
+
+</div>
+
+{{--                <div class="md:w-1/3 md:px-4 xl:px-6 mt-8 md:mt-0 text-center">--}}
+{{--                    <span class="w-20 border-t-2 border-solid border-indigo-200 inline-block mb-3"></span>--}}
+{{--                    <h5 class="text-xl font-medium uppercase mb-4">Perfect Tool</h5>--}}
+{{--                    <p class="text-gray-600">FWR blocks is a perfect tool for designers, developers and agencies looking--}}
+{{--                        to create stunning websites in no time.</p>--}}
+{{--                </div>--}}
